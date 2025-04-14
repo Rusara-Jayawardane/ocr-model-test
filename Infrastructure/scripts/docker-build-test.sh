d7940c25577efe1457373155d1b3418fb7adb592
Rusara Jayawardane <roosarajayawardane@gmail.com>
2025-04-14 20:08:10 +0530

    updated test script

--- Infrastructure/scripts/docker-build-test.sh
+++ Infrastructure/scripts/docker-build-test.sh
@@ -19,13 +19,29 @@ docker_build_and_push() {
 docker_tests() {
   echo "running docker tests ..."
   docker-compose -f Infrastructure/docker/docker-compose.yaml up -d
-  sleep 20
+  sleep 10
+
+  test_image_path=$(readlink -f docs/img/architecture-diagram.png)
+  response_code="$(curl -X POST http://localhost:8001/gateway/ocr -o /dev/null -s -w "%{http_code}\n" -F "image_file=@$test_image_path" || true)"
+  echo "$response_code"
+
+  if [ "$response_code" -eq 200 ]; then
+    echo "Success: Received 200 OK"
+    echo "Tests passed !"
+  else
+    echo "Failure: Received $response_code"
+    echo "Tests failed !"
+    docker-compose -f Infrastructure/docker/docker-compose.yaml down
+    exit 1
+  fi
   # we can run tests here such as checking the response of health endpoints
+
   docker-compose -f Infrastructure/docker/docker-compose.yaml down
 }
 
 for service in "${services[@]}"
 do
   docker_build_and_push "${service}"
-  docker_tests
 done
+
+docker_tests
